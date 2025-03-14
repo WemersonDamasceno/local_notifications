2f907db43a118de00faf540e3b5fddeb4f552a53
Wemerson Damasceno <wemersondamasceno7@gmail.com>
2025-03-14 13:53:17 -0300

    test

--- lib/views/url_launcher/url_launcher_page.dart
+++ lib/views/url_launcher/url_launcher_page.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:notifications_firebase/views/inputs/formatters/phone_email_formatter/phone_or_email_formatter.dart';
+import 'package:notifications_firebase/views/widgets/custom_input_widget.dart';
 import 'package:url_launcher/url_launcher.dart';
 
 class UrlLauncherPage extends StatefulWidget {
@@ -15,27 +17,49 @@ class _UrlLauncherPageState extends State<UrlLauncherPage> {
       appBar: AppBar(
         title: const Text('Url Launcher Page'),
       ),
-      body: SingleChildScrollView(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: [
-            const Center(child: Text('Pagina na internet')),
-            const SizedBox(height: 32),
-            ElevatedButton(
-              onPressed: () async {
-                if (!await launchUrl(
-                  Uri.parse(
-                      'https://serasaempreendedor.com.br/cadastro-deslogado'),
-                )) {
-                  throw Exception('Could not launch');
-                }
-              },
-              child: const Text('Regatar Senha'),
+      body: LayoutBuilder(
+        builder: (context, constraints) {
+          return SingleChildScrollView(
+            child: ConstrainedBox(
+              constraints: BoxConstraints(
+                minHeight: constraints.maxHeight,
+              ),
+              child: IntrinsicHeight(
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    const Spacer(),
+                    Padding(
+                      padding: const EdgeInsets.only(bottom: 16.0),
+                      child: ElevatedButton(
+                        onPressed: launcherUrl,
+                        child: const Text('Regatar Senha'),
+                      ),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.symmetric(horizontal: 16),
+                      child: CustomInputWidget(
+                        label: 'Fone/Email',
+                        formatters: [PhoneOrEmailFormatter()],
+                        controller: TextEditingController(),
+                      ),
+                    ),
+                    const SizedBox(height: 24),
+                  ],
+                ),
+              ),
             ),
-          ],
-        ),
+          );
+        },
       ),
     );
+  }
+
+  void launcherUrl() async {
+    if (!await launchUrl(
+      Uri.parse('https://serasaempreendedor.com.br/cadastro-deslogado'),
+    )) {
+      throw Exception('Could not launch');
+    }
   }
 }
